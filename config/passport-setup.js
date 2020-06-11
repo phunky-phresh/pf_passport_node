a64088b44d39dc7028a689a15c873677b2570991
ncooney94 <nick.cooney@gmail.com>
2020-06-11 16:49:09 +1000

    first commit

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,8 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 require('dotenv').config();
 
+const fetch = require( "node-fetch" );
+
 // const keys = require('../.env');
 
 passport.use( 
@@ -10,11 +12,42 @@ passport.use(
     callbackURL: '/auth/google/redirect',
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
-  }, (accessToken, refreshToken, profile, done) => {
+  }, async (accessToken, refreshToken, profile, done) => {
     // passport callback function
     console.log('passport callback function fired');
-    console.log(profile);
+    console.log(profile);  
+    const id = profile.id;
+    const name = profile.displayName;
+    const res = await hasuraRequest( INSERT, { objects: [{ id: id, email: name }]});  
+    console.log(res);
+    console.log(accessToken);
     
     })
 );
 
+
+const hasuraRequest = async ( query, variables ) => {
+	const response = await fetch( "https://hasuraql-pf.herokuapp.com/v1/graphql", {
+		method: "POST",
+		headers: {
+			"Accept": "application/json",
+			"Content-Type": "application/json",
+			"X-Hasura-Admin-Secret": process.env.HASURA_SECRET,
+		},
+		body: JSON.stringify({
+			query,
+			variables,
+		}),
+	});
+	return await response.json();
+};
+
+const INSERT = `
+mutation insertUser ( $objects: [users_insert_input!]! ) {
+    insert_users ( 
+        objects: $objects, 
+        on_conflict: { constraint: users_pkey, update_columns: email }
+    ) {
+        returning { id }
+    }
+}`;
